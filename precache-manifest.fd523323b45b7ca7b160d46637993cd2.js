9878d5d08fb3378992ec7fcc235762c73562c3ee
Andy Holt <andyholt85@gmail.com>
2020-10-25 23:54:15 -0600

    Updates

--- precache-manifest.fd523323b45b7ca7b160d46637993cd2.js
+++ precache-manifest.fd523323b45b7ca7b160d46637993cd2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1bd752d6b3e2559d5b127d18b4ccedd",
+    "revision": "f2fefdc5328c9b44e5a12ed54e5b784a",
     "url": "/resume_builder/index.html"
   },
   {
-    "revision": "fa4484b24c46a9aa7fd1",
+    "revision": "1861ef545c0b31dc57bd",
     "url": "/resume_builder/static/css/main.c2f03efa.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume_builder/static/js/2.93672cce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa4484b24c46a9aa7fd1",
-    "url": "/resume_builder/static/js/main.0eee3715.chunk.js"
+    "revision": "1861ef545c0b31dc57bd",
+    "url": "/resume_builder/static/js/main.024125a1.chunk.js"
   },
   {
     "revision": "0e57a87c9af79f642084",
