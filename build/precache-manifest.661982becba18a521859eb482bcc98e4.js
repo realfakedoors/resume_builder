9314e752c0ae1d01eb5f51cc79fa450fc08954aa
Andy Holt <andyholt85@gmail.com>
2020-10-25 23:45:04 -0600

    add build folder

---
+++ build/precache-manifest.661982becba18a521859eb482bcc98e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb4ab601704010670a0418e763ec318",
+    "url": "/realfakedoors/resume_builder/index.html"
+  },
+  {
+    "revision": "fa4484b24c46a9aa7fd1",
+    "url": "/realfakedoors/resume_builder/static/css/main.c2f03efa.chunk.css"
+  },
+  {
+    "revision": "aa6f0f08054bd3db05c5",
+    "url": "/realfakedoors/resume_builder/static/js/2.93672cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/realfakedoors/resume_builder/static/js/2.93672cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4484b24c46a9aa7fd1",
+    "url": "/realfakedoors/resume_builder/static/js/main.0eee3715.chunk.js"
+  },
+  {
+    "revision": "01a8c1dc05d1cc867fa3",
+    "url": "/realfakedoors/resume_builder/static/js/runtime-main.93a2f934.js"
+  }
+]);
